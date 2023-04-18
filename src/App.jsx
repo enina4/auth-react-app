b57eece13bc1627cd99c574c14facab750da6343
enina4 <enina4@umsa.bo>
2023-04-18 16:17:57 -0400

    configuracion inicial

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//layout
+import AuthLayout from "./layouts/auth/AuthLayout";
+
+//pages
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgetPassword from "./pages/auth/ForgetPassword";
+import ChangePassword from "./pages/auth/ChangePassword";
+import Error404 from "./pages/404";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Login />}></Route>
+          <Route path="register" element={<Register />}></Route>
+          <Route path="forget-password" element={<ForgetPassword />}></Route>
+          <Route path="change-password:/token" element={<ChangePassword />}></Route>
+        </Route>
+        <Route path="*" element={<Error404/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
